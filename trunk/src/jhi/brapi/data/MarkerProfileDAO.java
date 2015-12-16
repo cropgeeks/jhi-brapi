88cc6915e9d02871dbdea12f6be7adfe5919748e
imilne <imilne@d1b10728-f323-11e4-b4f1-f7877034998b>
2015-12-16 13:36:26 +0000

    - CHG: Continuing the same changes; this time on /markerprofiles/methods. - CHG: Removed more non-used list wrapper classes.

--- trunk/src/jhi/brapi/data/MarkerProfileDAO.java
+++ trunk/src/jhi/brapi/data/MarkerProfileDAO.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by gs40939 on 19/05/2015.
@@ -27,15 +28,13 @@ public class MarkerProfileDAO
 			"genotypes.germinatebase_id, genotypes.dataset_id from genotypes INNER JOIN markers ON genotypes.marker_id = " +
 			"markers.id INNER JOIN datasets ON genotypes.dataset_id = datasets.id";
 
-	public MarkerProfileList getAll()
+	public List<BrapiMarkerProfile> getAll()
 	{
 		try (Connection con = Database.INSTANCE.getDataSource().getConnection();
 			 PreparedStatement markerProfileStatement = con.prepareStatement(allMarkerProfiles);
 			 ResultSet resultSet = markerProfileStatement.executeQuery())
 		{
-			MarkerProfileList profiles = getProfiles(resultSet);
-
-			return profiles;
+			return getProfiles(resultSet);
 		}
 		catch (SQLException e) { e.printStackTrace(); }
 
@@ -46,8 +45,8 @@ public class MarkerProfileDAO
 	 * Return a specific Map specified by the supplied id. Queries a database using the allMarkers query specified
 	 * above.
 	 *
-	 * @param id	The id of the MarkerProfile to getJson
-	 * @return		A MarkerProfile object identified by id (or null if none exists).
+	 * @param id	The id of the BrapiMarkerProfile to getJson
+	 * @return		A BrapiMarkerProfile object identified by id (or null if none exists).
 	 */
 	public MarkerProfileData getById(String id)
 	{
@@ -79,24 +78,22 @@ public class MarkerProfileDAO
 		return statement;
 	}
 
-	// Given a ResultSet generated from the allMarkers query, returns a MarkerProfile object which has been initialized
+	// Given a ResultSet generated from the allMarkers query, returns a BrapiMarkerProfile object which has been initialized
 	// with the information from the ResultSet
-	private MarkerProfileList getProfiles(ResultSet resultSet)
+	private List<BrapiMarkerProfile> getProfiles(ResultSet resultSet)
 		throws SQLException
 	{
-		MarkerProfileList profiles = new MarkerProfileList();
-
-		HashMap<String, MarkerProfile> markerProfiles = new HashMap<>();
+		HashMap<String, BrapiMarkerProfile> markerProfiles = new HashMap<>();
 		HashMap<String, Integer> markerProfileCounts = new HashMap<>();
 
 		while (resultSet.next())
 		{
 			String markerprofileId = resultSet.getInt("dataset_id") + "-" + resultSet.getInt("germinatebase_id");
 			String germplasmId = resultSet.getString("germinatebase_id");
-			MarkerProfile profile = markerProfiles.get(markerprofileId);
+			BrapiMarkerProfile profile = markerProfiles.get(markerprofileId);
 			if (profile == null)
 			{
-				profile = new MarkerProfile();
+				profile = new BrapiMarkerProfile();
 				profile.setMarkerprofileId(markerprofileId);
 				profile.setGermplasmId(germplasmId);
 				markerProfiles.put(markerprofileId, profile);
@@ -110,12 +107,11 @@ public class MarkerProfileDAO
 				markerProfiles.get(markerprofileId).setResultCount(count);
 			}
 		}
-		profiles.setMarkerprofiles(new ArrayList<>(markerProfiles.values()));
 
-		return profiles;
+		return new ArrayList<>(markerProfiles.values());
 	}
 
-	// Given a ResultSet generated from the allMarkers query, returns a MarkerProfile object which has been initialized
+	// Given a ResultSet generated from the allMarkers query, returns a BrapiMarkerProfile object which has been initialized
 	// with the information from the ResultSet
 	private MarkerProfileData getProfile(ResultSet resultSet)
 		throws SQLException
@@ -134,11 +130,11 @@ public class MarkerProfileDAO
 	}
 
 	/**
-	 * Return a count of the Markers which are contained within the MarkerProfile in the form of a MarkerProfileCount.
+	 * Return a count of the Markers which are contained within the BrapiMarkerProfile in the form of a MarkerProfileCount.
 	 * Uses the markerCount query defined above to getJson the data from the database.
 	 *
-	 * @param id	The id of the MarkerProfile for which a count is desired.
-	 * @return		The MarkerProfileCount for the MarkerProfile with the given id.
+	 * @param id	The id of the BrapiMarkerProfile for which a count is desired.
+	 * @return		The MarkerProfileCount for the BrapiMarkerProfile with the given id.
 	 */
 	public MarkerProfileCount getCountById(String id)
 	{
