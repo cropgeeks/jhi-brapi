8570e0692ab44f9e3cbf25cd486f1583e5d40d9b
srauba <srauba@d1b10728-f323-11e4-b4f1-f7877034998b>
2016-08-03 13:58:58 +0000

    - BUG: Fixed copy/paste error. Allele2 value should have been checked but allele1 was checked.

--- trunk/src/jhi/brapi/data/GenotypeEncodingUtils.java
+++ trunk/src/jhi/brapi/data/GenotypeEncodingUtils.java
@@ -9,14 +9,14 @@ public class GenotypeEncodingUtils
 {
 	public static String getString(String allele1, String allele2, GenotypeEncodingParams params)
 	{
-		allele1 = fixAllele(allele1, params.getUnknownString()); // Replace empty string and dash into the unknownString
-		allele2 = fixAllele(allele2, params.getUnknownString()); // Replace empty string and dash into the unknownString
+		allele1 = fixAllele(allele1, params.getUnknownString()); // Replace empty string and dash with the unknownString
+		allele2 = fixAllele(allele2, params.getUnknownString()); // Replace empty string and dash with the unknownString
 
-		if (Objects.equals(allele1, params.getUnknownString()) && Objects.equals(allele2, params.getUnknownString())) // If both are unknown return empty string
-			return "";
+		if (Objects.equals(allele1, params.getUnknownString()) && Objects.equals(allele2, params.getUnknownString())) // If both are unknown return unknownString
+			return params.getUnknownString(); // TODO: check if this is correct or if this should return an empty string
 		else if (Objects.equals(allele1, params.getUnknownString())) // If only the first is unknown, return the second
 			return allele2;
-		else if (Objects.equals(allele1, params.getUnknownString())) // If only the second is unknown, return the first
+		else if (Objects.equals(allele2, params.getUnknownString())) // If only the second is unknown, return the first
 			return allele1;
 		else if (Objects.equals(allele1, allele2)) // If both are the same, return the first
 		{
