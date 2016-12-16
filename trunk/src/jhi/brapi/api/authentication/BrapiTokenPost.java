85281c4b1d668877d4da7a82cf71ae7479b95c55
gsteph <gsteph@d1b10728-f323-11e4-b4f1-f7877034998b>
2016-12-16 10:33:01 +0000

    - CHG: Added a constructor to BrapiTokenPost to more easily populate the bean with data. - NEW: Added BrapiMarkerProfilePost to support posting to the markerprofile-search REST endpoint.

--- trunk/src/jhi/brapi/api/authentication/BrapiTokenPost.java
+++ trunk/src/jhi/brapi/api/authentication/BrapiTokenPost.java
@@ -11,6 +11,18 @@ public class BrapiTokenPost
 	private String grant_type;
 	private String client_id;
 
+	public BrapiTokenPost()
+	{
+	}
+
+	public BrapiTokenPost(String username, String password, String grant_type, String client_id)
+	{
+		this.username = username;
+		this.password = password;
+		this.grant_type = grant_type;
+		this.client_id = client_id;
+	}
+
 	public String getUsername()
 		{ return username; }
 
