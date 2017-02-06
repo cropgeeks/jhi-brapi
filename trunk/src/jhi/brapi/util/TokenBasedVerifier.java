4ae89ac4d6323fd67975e268bd8962663f67e50a
gsteph <gsteph@d1b10728-f323-11e4-b4f1-f7877034998b>
2017-02-06 16:51:37 +0000

    - CHG: Tweaked token verification code to give more appropriate http code responses when not presented with auth details.

--- trunk/src/jhi/brapi/util/TokenBasedVerifier.java
+++ trunk/src/jhi/brapi/util/TokenBasedVerifier.java
@@ -18,20 +18,30 @@ public class TokenBasedVerifier implements Verifier
 	{
 		ChallengeResponse cr = request.getChallengeResponse();
 
-		// Check if our token bearer scheme is being used by the client
-		if (ChallengeScheme.HTTP_OAUTH_BEARER.equals(cr.getScheme()))
+		System.out.println("cr: " + cr);
+
+		try
 		{
-			// If there's no token return result_missing
-			final String bearer = cr.getRawValue();
-			if (bearer == null || bearer.isEmpty())
-				return RESULT_MISSING;
 
-			// Otherwise check to see if we have the given token in our sessions
-			// hash
-			if (Brapi.getSessions().isValid(bearer))
-				return RESULT_VALID;
+			// Check if our token bearer scheme is being used by the client
+			if (ChallengeScheme.HTTP_OAUTH_BEARER.equals(cr.getScheme()))
+			{
+				// If there's no token return result_missing
+				final String bearer = cr.getRawValue();
+				if (bearer == null || bearer.isEmpty())
+					return RESULT_MISSING;
 
-			// Otherwise we have an invalid token
+				// Otherwise check to see if we have the given token in our sessions
+				// hash
+				if (Brapi.getSessions().isValid(bearer))
+					return RESULT_VALID;
+
+				// Otherwise we have an invalid token
+				return RESULT_INVALID;
+			}
+		}
+		catch (Exception e)
+		{
 			return RESULT_INVALID;
 		}
 
