fa0ffa9391a72725f057e9d35a904dc63bfe2897
gsteph <gsteph@d1b10728-f323-11e4-b4f1-f7877034998b>
2017-06-13 18:05:15 +0000

    - NEW: Added classes to support the new /allelematrices endpoint.

---
+++ trunk/src/jhi/brapi/api/allelematrices/BrapiAlleleMatrixDataset.java
@@ -0,0 +1,42 @@
+package jhi.brapi.api.allelematrices;
+
+import java.sql.Date;
+
+public class BrapiAlleleMatrixDataset
+{
+	private String name;
+	private String matrixDbId;
+	private String description;
+	private Date lastUpdated;
+	private String studyDbId;
+
+	public String getName()
+		{ return name; }
+
+	public void setName(String name)
+		{ this.name = name; }
+
+	public String getMatrixDbId()
+		{ return matrixDbId; }
+
+	public void setMatrixDbId(String matrixDbId)
+		{ this.matrixDbId = matrixDbId; }
+
+	public String getDescription()
+		{ return description; }
+
+	public void setDescription(String description)
+		{ this.description = description; }
+
+	public Date getLastUpdated()
+		{ return lastUpdated; }
+
+	public void setLastUpdated(Date lastUpdated)
+		{ this.lastUpdated = lastUpdated; }
+
+	public String getStudyDbId()
+		{ return studyDbId; }
+
+	public void setStudyDbId(String studyDbId)
+		{ this.studyDbId = studyDbId; }
+}
