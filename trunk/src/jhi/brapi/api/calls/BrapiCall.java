1bb98838bbfb9503999b2b9ae501b975d2fb40fe
gsteph <gsteph@d1b10728-f323-11e4-b4f1-f7877034998b>
2017-06-16 17:53:37 +0000

    - NEW: Added explicit methods for method and datatype building to the BrapiCall builder.

--- trunk/src/jhi/brapi/api/calls/BrapiCall.java
+++ trunk/src/jhi/brapi/api/calls/BrapiCall.java
@@ -4,6 +4,15 @@ import java.util.*;
 
 public class BrapiCall
 {
+	public static final String DATATYPE_JSON = "json";
+	public static final String DATATYPE_TSV = "tsv";
+	public static final String DATATYPE_FLAPJACK = "flapjack";
+
+	public static final String METHOD_GET = "GET";
+	public static final String METHOD_POST = "POST";
+	public static final String METHOD_PUT = "PUT";
+	public static final String METHOD_DELETE = "DELETE";
+
 	private String call;
 	private List<String> datatypes = new ArrayList<>();
 	// HTTP methods (e.g. POST, GET, etc)
@@ -26,6 +35,41 @@ public class BrapiCall
 	{
 		datatypes.add(dataType);
 		return this;
+	}
+
+	public BrapiCall withDatatypeJson()
+	{
+		return addDatatype(DATATYPE_JSON);
+	}
+
+	public BrapiCall withDatatypeTsv()
+	{
+		return addDatatype(DATATYPE_TSV);
+	}
+
+	public BrapiCall withDatatypeFlapjack()
+	{
+		return addDatatype(DATATYPE_FLAPJACK);
+	}
+
+	public BrapiCall withMethodGet()
+	{
+		return addMethod(METHOD_GET);
+	}
+
+	public BrapiCall withMethodPost()
+	{
+		return addMethod(METHOD_POST);
+	}
+
+	public BrapiCall withMethodPut()
+	{
+		return addMethod(METHOD_PUT);
+	}
+
+	public BrapiCall withMethodDelete()
+	{
+		return addMethod(METHOD_DELETE);
 	}
 
 	public boolean hasDataType(String dataType)
